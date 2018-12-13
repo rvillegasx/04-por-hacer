860ac609a0a3581aa761227b6dd7f11c934d0717
Roberto Villegas <rvillegas@gmail.com>
2018-12-12 19:10:11 -0600

    Primer commit

---
+++ README.md
@@ -0,0 +1,9 @@
+## Aplicación de comandos
+
+Este es el ejercicio 4 del curso
+
+Recuerden instalar los paquetes de node
+
+```
+npm install
+```
---
+++ por-hacer/por-hacer.js
@@ -0,0 +1,75 @@
+const fs = require('fs');
+
+let listadoPorHacer = [];
+
+const guardarDB = () => {
+    let data = JSON.stringify(listadoPorHacer);
+    fs.writeFile('db/data.json', data, (err) => {
+        if (err) throw new Error( 'No se pudo grabar la data' , err );
+    });
+};
+
+const cargarDB = () => {
+    try {
+        listadoPorHacer = require('../db/data.json');
+    } catch (error) {
+        listadoPorHacer = [];
+    }
+};
+
+const getListado = () =>{
+    cargarDB();
+    return listadoPorHacer;
+};
+
+const crear = (descripcion) => {
+
+    cargarDB();
+
+    let porHacer = {
+        descripcion,
+        completado: false
+    };
+
+    listadoPorHacer.push(porHacer);
+
+    guardarDB();
+
+    return porHacer;
+
+};
+
+const actualizar = (descripcion, completado = true) => {
+    cargarDB();
+    let index = listadoPorHacer.findIndex(tarea => {
+        return tarea.descripcion === descripcion;
+    });
+    if (index >= 0) {
+        listadoPorHacer[index].completado = completado;
+        guardarDB();
+        return true;
+    } else {
+        return false;
+    }
+};
+
+const borrar = (descripcion) => {
+    cargarDB();
+    nuevoListado = listadoPorHacer.filter(tarea => {
+        return tarea.descripcion !== descripcion;
+    });
+    if ( listadoPorHacer.length  === nuevoListado. length ) {
+        return false;
+    }else{
+        listadoPorHacer = nuevoListado;
+        guardarDB();
+        return true;
+    }
+};
+
+module.exports = {
+    crear,
+    getListado,
+    actualizar,
+    borrar
+};
